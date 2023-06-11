1a73c0f1b537cc723b8fe5b2c79c8b75dccb80a6
the_fallen_27 <mariopgsco@gmail.com>
2023-06-11 17:14:25 +0300

    Updated the cpp algorithm to include contraction hierarchies. Input and readme changed accordingly

--- dijkstra/cpp/dijkstra.cpp
+++ dijkstra/cpp/dijkstra.cpp
@@ -4,101 +4,101 @@
 #include <queue>
 #include <vector>
 
-constexpr int INF = std::numeric_limits<int>::max();
-
-struct edge 
+struct edge
 {
     int destination;
     int weight;
 };
 
-void 
-ReadGraphFromFile(const std::string& filename, std::vector<std::vector<edge>>& graph) 
+struct node
+{
+    std::vector<edge> neighbours;
+};
+
+std::vector<node>
+ReadGraphFromFile(const std::string& filename, int& number_of_nodes)
 {
     std::ifstream file(filename);
-    if (!file) 
+    if (!file)
     {
         std::cerr << "Error opening file " << filename << std::endl;
         exit(1);
     }
 
-    int number_of_nodes, number_of_edges;
-    file >> number_of_nodes >> number_of_edges;
+    file >> number_of_nodes;
 
-    graph.resize(number_of_nodes);
+    std::vector<node> graph(number_of_nodes);
+    int source, destination, weight;
 
-    for (int i = 0; i < number_of_edges; ++i) 
+    while (file >> source >> destination >> weight)
     {
-        int source, destination, weight;
-        file >> source >> destination >> weight;
-
-        graph[source].push_back({ destination, weight });
+        graph[source].neighbours.push_back({ destination, weight });
         // Uncomment below line if you want the graph to be undirected
-        // graph[destination].push_back({ source, weight });
+        // graph[destination].neighbours.push_back({ source, weight });
     }
 
-    file.close();
+    return graph;
 }
 
-void 
-Dijkstra(const std::vector<std::vector<edge>>& graph, int start_node, std::vector<int>& distances) 
+std::vector<int>
+Dijkstra(const std::vector<node>& graph, int start_node)
 {
-    long long int number_of_nodes = graph.size();
-    distances.assign(number_of_nodes, INF);
+    std::vector<int> distances(graph.size(), std::numeric_limits<int>::max());
     distances[start_node] = 0;
 
     std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> pr_queue;
     pr_queue.push({ 0, start_node });
 
-    while (!pr_queue.empty()) 
+    while (!pr_queue.empty())
     {
-        int dist = pr_queue.top().first;
-        int u = pr_queue.top().second;
+        int distance = pr_queue.top().first;
+        int current = pr_queue.top().second;
         pr_queue.pop();
 
-        if (dist > distances[u]) 
+        if (distance > distances[current])
         {
             continue;
         }
 
-        for (const auto& edge : graph[u]) 
+        for (const auto& graph_edge : graph[current].neighbours)
         {
-            int v = edge.destination;
-            int weight = edge.weight;
+            int neighbour = graph_edge.destination;
+            int new_distance = distance + graph_edge.weight;
 
-            if (dist + weight < distances[v]) 
+            if (new_distance < distances[neighbour])
             {
-                distances[v] = dist + weight;
-                pr_queue.push({ distances[v], v });
+                distances[neighbour] = new_distance;
+                pr_queue.push({ new_distance, neighbour });
             }
         }
     }
+
+    return distances;
 }
 
-int main() 
+int main()
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     std::string filename = "dijkstra_input.txt";
 
-    std::vector<std::vector<edge>> graph;
-    ReadGraphFromFile(filename, graph);
-
+    int number_of_nodes;
+    std::vector<node> graph = ReadGraphFromFile(filename, number_of_nodes);
+    
     int start_node;
     std::cout << "Enter the starting node: ";
     std::cin >> start_node;
 
-    std::vector<int> distances;
-    Dijkstra(graph, start_node, distances);
+    std::vector<int> distances = Dijkstra(graph, start_node);
 
     std::cout << "Shortest paths from node " << start_node << ":" << std::endl;
-    for (int i = 0; i < distances.size(); ++i) 
+    for (int i = 0; i < number_of_nodes; ++i)
     {
-        if (distances[i] == INF) 
+        if (distances[i] == std::numeric_limits<int>::max())
         {
             std::cout << "No path from node " << start_node << " to node " << i << std::endl;
         }
-        else 
+        else
         {
             std::cout << "Node " << i << ": Distance = " << distances[i] << std::endl;
         }
